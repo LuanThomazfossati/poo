73b06274ea16d15efd4b1d891f7de3ad62a62e69
Luan Thomaz Fossati <luangatissimo09@gmail.com>
2026-05-20 16:46:04 -0300

    Update conta.py

--- conta.py
+++ conta.py
@@ -1,9 +1,10 @@
 class conta:
       def __init__(self,saldo)
-          self.saldo = saldo
+          self.saldo = saldo  
       def saca(self,valor)
-          valor = x 
-          saldo - x = saldofinal
-      def depositar(self,deposita)
-          deposita = y 
-          y + saldofinal = saldo final
+          self.saldo -= valor
+      def depositar(self,deposita) 
+          self.saldo += deposita
+      def calcularRendimento(self)
+          self.saldo *= 0,1
+          
