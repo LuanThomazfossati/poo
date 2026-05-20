8a8571a2546385c3da512e5d3af5ce949fc68677
João Victor Farias <joao.mirandafarias16@gmail.com>
2026-05-20 17:04:33 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
 from conta import Conta
-
+obj = Conta(1000)
+print("(1) sacar")
+print("(2) depositar")
+print("(3) calcular rendimento")
+opc = input("Qual opção você vai escolher?")
+if opc == "1":
+  valor_sacar = int(input("Qual o valor do saque?"))
+  obj.sacar(valor_sacar)
+elif opc == "2":
+  valor_depositar = int(input("Qual o valor do depósito?"))
+  obj.depositar(valor_depositar)
+elif opc == "3":
+  print(obj.calcularRendimento())
+  
