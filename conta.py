6e787afd78ce02cbbf7aa899726dfc002902bce5
Luan Thomaz Fossati <luangatissimo09@gmail.com>
2026-05-20 16:36:41 -0300

    Update conta.py

--- conta.py
+++ conta.py
@@ -1,1 +1,9 @@
 class conta:
+      def __init__(self,saldo)
+          self.saldo = saldo
+      def saca(self,valor)
+          valor = x 
+          x - saldo = saldofinal
+      def depositar(self,deposita)
+          deposita = y 
+          y + saldofinal = saldo final
