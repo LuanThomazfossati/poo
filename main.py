efe13930c03389a89f4177d56086504fb1e5f55b
João Victor Farias <joao.mirandafarias16@gmail.com>
2026-05-20 17:16:55 -0300

    poo_trio

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 from conta import Conta
 obj = Conta(1000)
-print("(1) sacar")
-print("(2) depositar")
-print("(3) calcular rendimento")
-opc = input("Qual opção você vai escolher?")
-if opc == "1":
-  valor_sacar = int(input("Qual o valor do saque?"))
-  obj.sacar(valor_sacar)
-elif opc == "2":
-  valor_depositar = int(input("Qual o valor do depósito?"))
-  obj.depositar(valor_depositar)
-elif opc == "3":
-  print(obj.calcularRendimento())
+while True:
+  print(obj.saldo)
+  print("(1) sacar")
+  print("(2) depositar")
+  print("(3) calcular rendimento")
+  print("(4) sair")
+  opc = input("Qual opção você vai escolher?")
+  if opc == "1":
+    valor_sacar = int(input("Qual o valor do saque?"))
+    obj.sacar(valor_sacar)
+  elif opc == "2":
+    valor_depositar = int(input("Qual o valor do depósito?"))
+    obj.depositar(valor_depositar)
+  elif opc == "3":
+    print(obj.calcularRendimento())
+  elif opc == "4":
+    break
   
