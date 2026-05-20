bcd13aced60f7a71af0d86dcc3fef02fd702d67c
godwers <gondes.historia@gmail.com>
2026-05-20 20:49:02 -0300

    mudei um pouco do main.py pra deixar mais bonito e removi essa bosta do __pycache__

--- main.py
+++ main.py
@@ -2,16 +2,16 @@ from conta import Conta
 obj = Conta(1000)
 while True:
   print(obj.saldo)
-  print("(1) sacar")
-  print("(2) depositar")
-  print("(3) calcular rendimento")
-  print("(4) sair")
-  opc = input("Qual opção você vai escolher?")
+  print("1) Sacar")
+  print("2) Depositar")
+  print("3) Calcular rendimento")
+  print("4) Sair")
+  opc = input("Qual opção você vai escolher? ")
   if opc == "1":
-    valor_sacar = int(input("Qual o valor do saque?"))
+    valor_sacar = int(input("Qual o valor do saque? "))
     obj.sacar(valor_sacar)
   elif opc == "2":
-    valor_depositar = int(input("Qual o valor do depósito?"))
+    valor_depositar = int(input("Qual o valor do depósito? "))
     obj.depositar(valor_depositar)
   elif opc == "3":
     print(obj.calcularRendimento())
